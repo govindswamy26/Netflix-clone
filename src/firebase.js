d372825f2857ec22d6f5f6f8b9d3e20bc034319f
Govind swamy <govindswamy567@gmail.com>
2025-08-06 21:56:15 +0530

    done with netfilx-clone2

--- src/firebase.js
+++ src/firebase.js
@@ -25,7 +25,7 @@ const db = getFirestore(app);
 const signup = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user();
+    const user = res.user;
     await addDoc(collection(db, "user"), {
       uid: user.uid,
       name,
